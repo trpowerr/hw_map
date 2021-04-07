241f4f7e9ee81cd26aa81617d457f3e07a7900e1
Tryakin Mikhail <mixazbubl@yandex.ru>
2021-04-07 15:59:33 +0300

    ver 0.1

--- src/js/app.js
+++ src/js/app.js
@@ -1,62 +1,18 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-restricted-syntax */
-/* eslint-disable max-classes-per-file */
-class Character {
-  constructor(name) {
-    this.name = name;
-    this.health = 100;
+export default class ErrorRepository {
+  constructor () {
+      this.errors = new Map();
+      this.errors.set(111, 'Error111');
+      this.errors.set(222, 'Error222');
+      this.errors.set(333, 'Error333');
+      this.errors.set(444, 'Error444');
   }
-}
-
-export class Bowman extends Character {
-  constructor(name) {
-    super(name);
-    this.attack = 25;
-    this.defence = 25;
-  }
-}
-
-export class Swordman extends Character {
-  constructor(name) {
-    super(name);
-    this.attack = 40;
-    this.defence = 10;
-  }
-}
-
-export class Magician extends Character {
-  constructor(name) {
-    super(name);
-    this.attack = 10;
-    this.defence = 40;
-  }
-}
-
-export default class Team {
-  constructor() {
-    this.members = new Set();
-  }
-
-  add(Person) {
-    const newPerson = new Person('Misha');
-    this.members.add(newPerson);
-    return this.members;
-  }
-
-  addAll(...persons) {
-    for (const Person of persons) {
-      const newPerson = new Person('Misha');
-      this.members.add(newPerson);
-    }
-
-    return this.members;
-  }
-
-  toArray() {
-    const arrPerson = [];
-    for (const value of this.members) {
-      arrPerson.push(value);
-    }
-    return arrPerson;
+  
+  translate(code) {
+      if (this.errors.has(code)) {
+          return this.errors.get(code);
+      } else { 
+          console.log('Unknown error');
+      }
+      
   }
 }
